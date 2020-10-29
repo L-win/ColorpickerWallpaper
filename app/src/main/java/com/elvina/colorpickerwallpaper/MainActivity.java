faaf91ff8630d3e858d0ddd5011ba96d507d2668
L-win <l-win@inbox.ru>
2020-10-29 19:50:23 +0300

    colorpicker added

--- app/src/main/java/com/elvina/colorpickerwallpaper/MainActivity.java
+++ app/src/main/java/com/elvina/colorpickerwallpaper/MainActivity.java
@@ -1,14 +1,49 @@
 package com.elvina.colorpickerwallpaper;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class MainActivity extends AppCompatActivity {
+
+    ConstraintLayout layout;
+    int defaultColor;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout = findViewById(R.id.layout);
+        defaultColor = ContextCompat.getColor(MainActivity.this,R.color.design_default_color_primary);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openColorPicker();
+            }
+        });
+    }
+
+    public void openColorPicker(){
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, defaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                defaultColor = color;
+                layout.setBackgroundColor(defaultColor);
+            }
+        });
+        colorPicker.show();
     }
 }
