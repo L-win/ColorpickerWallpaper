7b933ebae104ba190421d994bc06e13176c1b67b
L-win <l-win@inbox.ru>
2020-10-29 20:50:20 +0300

    create bitmap from color

--- app/src/main/java/com/elvina/colorpickerwallpaper/MainActivity.java
+++ app/src/main/java/com/elvina/colorpickerwallpaper/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import yuku.ambilwarna.AmbilWarnaDialog;
 
@@ -15,13 +21,18 @@ public class MainActivity extends AppCompatActivity {
     ConstraintLayout layout;
     int defaultColor;
     Button button;
+    ImageView imageView;
+
+    Bitmap bitmap = null;
+    ColorDrawable cd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         layout = findViewById(R.id.layout);
-        defaultColor = ContextCompat.getColor(MainActivity.this,R.color.design_default_color_primary);
+        defaultColor = ContextCompat.getColor(MainActivity.this, R.color.design_default_color_primary);
+        imageView = findViewById(R.id.image);
         button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void openColorPicker(){
+    public void openColorPicker() {
         AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, defaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
             @Override
             public void onCancel(AmbilWarnaDialog dialog) {
@@ -42,8 +53,24 @@ public class MainActivity extends AppCompatActivity {
             public void onOk(AmbilWarnaDialog dialog, int color) {
                 defaultColor = color;
                 layout.setBackgroundColor(defaultColor);
+                setColorDrawable();
+                imageView.setImageBitmap(getbitmap());
             }
         });
         colorPicker.show();
     }
+
+    public Bitmap getbitmap() {
+        Bitmap bitmap = Bitmap.createBitmap(20, 20, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        cd.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        cd.draw(canvas);
+        return bitmap;
+    }
+
+    public void setColorDrawable() {
+        cd = new ColorDrawable(defaultColor);
+    }
+
+
 }
